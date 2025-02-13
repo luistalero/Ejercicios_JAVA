2507de798b1728cc975d5e826e9a1dd6de3c18a6
Camilomachuca <Camilomachuca03@gmail.com>
2025-02-13 16:09:57 -0500

    :tada:

---
+++ src/main/java/com/ejercicios/DepositoIntereses.java
@@ -0,0 +1,13 @@
+package com.ejercicios;
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class DepositoIntereses {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("######.00");
+
+        
+    }
+}
